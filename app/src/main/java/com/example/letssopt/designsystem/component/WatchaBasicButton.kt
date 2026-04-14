35cd1243d67ff9e9ce5fa66c9a8dbfaf4b40df30
oilbeaneda <20221444@sungshin.ac.kr>
2026-04-14 11:40:56 +0900

    [mod/#2] BasicButton BaseButton 파일 분리

--- app/src/main/java/com/example/letssopt/designsystem/component/WatchaBasicButton.kt
+++ app/src/main/java/com/example/letssopt/designsystem/component/WatchaBasicButton.kt
@@ -3,46 +3,62 @@ package com.example.letssopt.designsystem.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.letssopt.designsystem.style.ButtonStyle
+import com.example.letssopt.designsystem.style.WatchaBtnColor
 import com.example.letssopt.designsystem.theme.LETSSOPTTheme
-import com.example.letssopt.designsystem.theme.WatchaTheme
 import com.example.letssopt.designsystem.theme.WatchaTheme.typography
 import com.example.letssopt.extension.noRippleClickable
+
 
 @Composable
 fun WatchaBasicButton(
     buttonText: String,
+    buttonColor: WatchaBtnColor,
     onClick: () -> Unit,
+    textStyle: TextStyle,
+    contentPadding: PaddingValues,
+    shape: Shape,
     modifier: Modifier = Modifier,
     disabled: Boolean = true,
 ) {
+    val (backgroundColor, contentColor) =
+        if (!disabled) buttonColor.backgroundColor to buttonColor.textColor
+        else buttonColor.disabledBackgroundColor to buttonColor.disabledTextColor
+
     Box(
         modifier = modifier
             .background(
-                if (!disabled) WatchaTheme.colors.primaryRed else WatchaTheme.colors.disabled,
-                shape = RoundedCornerShape(8.dp)
+                color = backgroundColor,
+                shape = shape,
             )
             .noRippleClickable(
                 isEnabled = !disabled,
                 onClick = onClick
             )
-            .padding(horizontal = 97.dp, vertical = 16.dp),
+            .padding(contentPadding),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = buttonText,
-            style = typography.headline.head3B16,
-            color = WatchaTheme.colors.textPrimary,
+            style = textStyle,
+            color = contentColor,
         )
     }
 }
+
 
 @Preview
 @Composable
@@ -55,11 +71,20 @@ private fun WatchaBasicButtonPreview(
                 buttonText = "로그인",
                 onClick = {},
                 modifier = modifier,
+                buttonColor = ButtonStyle.PRIMARY.getButtonColor(),
+                textStyle = typography.headline.head3B16,
+                contentPadding = PaddingValues(horizontal = 97.dp, vertical = 16.dp),
+                shape = RoundedCornerShape(8.dp),
             )
+            Spacer(modifier = Modifier.height(16.dp))
             WatchaBasicButton(
                 buttonText = "로그인",
                 onClick = {},
                 modifier = modifier,
+                buttonColor = ButtonStyle.DISABLED.getButtonColor(),
+                textStyle = typography.headline.head3B16,
+                contentPadding = PaddingValues(horizontal = 97.dp, vertical = 16.dp),
+                shape = RoundedCornerShape(8.dp),
                 disabled = false,
             )
         }
