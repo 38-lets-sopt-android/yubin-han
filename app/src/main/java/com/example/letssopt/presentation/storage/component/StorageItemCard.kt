944c263a9bb986a28b3df6a1c176a7f1ba8d6b27
oilbeaneda <20221444@sungshin.ac.kr>
2026-04-19 01:45:11 +0900

    [ui/#4] StorageItemCard 구현

---
+++ app/src/main/java/com/example/letssopt/presentation/storage/component/StorageItemCard.kt
@@ -0,0 +1,68 @@
+package com.example.letssopt.presentation.storage.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.letssopt.R
+import com.example.letssopt.designsystem.theme.LETSSOPTTheme
+import com.example.letssopt.extension.noRippleClickable
+
+@Composable
+fun StorageItemCard(
+    imageRes: Int,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.width(103.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(103 / 153f)
+                .clip(RoundedCornerShape(10.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(9.dp))
+        Icon(
+            ImageVector.vectorResource(R.drawable.ic_close),
+            "삭제",
+            tint = Color.Unspecified,
+            modifier = Modifier
+                .noRippleClickable(onClick = onDeleteClick)
+                .size(24.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun StorageItemCardPreview() {
+    LETSSOPTTheme {
+        StorageItemCard(
+            imageRes = R.drawable.img_poster_love_translate,
+            onDeleteClick = { }
+        )
+    }
+}
