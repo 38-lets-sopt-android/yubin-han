4f1f0cf87f07fb4170e398332b1f49af1a528448
oilbeaneda <20221444@sungshin.ac.kr>
2026-04-29 22:40:31 +0900

    [feat/#6] AppDatabase 구현

---
+++ app/src/main/java/com/example/letssopt/core/data/local/database/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.letssopt.core.data.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.letssopt.core.data.local.dao.StoredItemDao
+import com.example.letssopt.core.data.local.entity.StoredItemEntity
+
+
+@Database(entities = [StoredItemEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun storedItemDao(): StoredItemDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "watcha_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
