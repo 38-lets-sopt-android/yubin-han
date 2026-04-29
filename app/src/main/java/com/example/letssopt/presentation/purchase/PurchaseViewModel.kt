56bf8668d12e6180891dc84586d609b02f2b2107
oilbeaneda <20221444@sungshin.ac.kr>
2026-04-30 08:55:46 +0900

    [add/#6] 더미데이터 추가

--- app/src/main/java/com/example/letssopt/presentation/purchase/PurchaseViewModel.kt
+++ app/src/main/java/com/example/letssopt/presentation/purchase/PurchaseViewModel.kt
@@ -21,6 +21,9 @@ class PurchaseViewModel(private val storedItemDao: StoredItemDao) : ViewModel() 
         PurchaseContent("이 사랑 통역 되나요?", R.drawable.img_poster_love_translate),
         PurchaseContent("기묘한 이야기5", R.drawable.img_poster_stranger_things),
         PurchaseContent("프로젝트 헤일메리", R.drawable.img_poster_hail_mary),
+        PurchaseContent("주토피아2", R.drawable.img_poster_jootopia2),
+        PurchaseContent("모아나2", R.drawable.img_poster_moana2),
+        PurchaseContent("어벤져스: 둠스데이", R.drawable.img_poster_avengers_doomsday)
     )
     val purchaseItems: List<PurchaseContent> = _purchaseItems
 
