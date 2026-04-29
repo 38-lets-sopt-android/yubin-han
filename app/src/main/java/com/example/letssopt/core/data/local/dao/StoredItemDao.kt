4fbffc0d2a26425aff0fe5d459e13f962b6bd5ee
oilbeaneda <20221444@sungshin.ac.kr>
2026-04-29 22:40:50 +0900

    [feat/#6] StoredItemDao 구현

---
+++ app/src/main/java/com/example/letssopt/core/data/local/dao/StoredItemDao.kt
@@ -0,0 +1,21 @@
+package com.example.letssopt.core.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.letssopt.core.data.local.entity.StoredItemEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface StoredItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: StoredItemEntity)
+
+    @Query("SELECT * FROM stored_items")
+    fun getAllStoredItems(): Flow<List<StoredItemEntity>>
+
+    @Delete
+    suspend fun deleteItem(item: StoredItemEntity)
+}
