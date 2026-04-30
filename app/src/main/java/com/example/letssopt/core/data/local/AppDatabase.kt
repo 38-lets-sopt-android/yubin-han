1cd30013a87cab56e4d813c0ae9d2b8a741d7e6b
oilbeaneda <20221444@sungshin.ac.kr>
2026-04-30 11:38:18 +0900

    [chore/#6] 패키지 구조 변경

--- app/src/main/java/com/example/letssopt/core/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/letssopt/core/data/local/AppDatabase.kt
@@ -1,4 +1,4 @@
-package com.example.letssopt.core.data.local.database
+package com.example.letssopt.core.data.local
 
 import android.content.Context
 import androidx.room.Database
@@ -7,11 +7,10 @@ import androidx.room.RoomDatabase
 import com.example.letssopt.core.data.local.dao.StoredItemDao
 import com.example.letssopt.core.data.local.entity.StoredItemEntity
 
-
 @Database(entities = [StoredItemEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun storedItemDao(): StoredItemDao
-    
+
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
