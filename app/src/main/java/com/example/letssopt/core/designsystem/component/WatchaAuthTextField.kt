784a23593612b37703f6f6a95192f49c03cabc98
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-04 10:56:18 +0900

    [refactor/#6]WatchaAuthTextField 라벨/텍스트스타일 추가

--- app/src/main/java/com/example/letssopt/core/designsystem/component/WatchaAuthTextField.kt
+++ app/src/main/java/com/example/letssopt/core/designsystem/component/WatchaAuthTextField.kt
@@ -2,15 +2,21 @@ package com.example.letssopt.core.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,33 +26,47 @@ import com.example.letssopt.core.designsystem.theme.WatchaTheme
 
 @Composable
 fun WatchaAuthTextField(
-    textFieldStyle: TextFieldStyle,
-    placeholder: String,
+    label: String,
     value: String,
+    placeholder: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    visualTransformation: VisualTransformation= VisualTransformation.None,
+    textFieldStyle: TextFieldStyle = if (value.isNotBlank()) TextFieldStyle.INPUT else TextFieldStyle.DISABLED,
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(
+        imeAction = imeAction,
+        keyboardType = KeyboardType.Text
+    ),
+    visualTransformation: VisualTransformation = VisualTransformation.None,
     enabled: Boolean = true,
 ) {
-    Box(
-        modifier = modifier
-            .background(
-                color = textFieldStyle.getTextFieldColor().backgroundColor,
-                shape = RoundedCornerShape(8.dp),
-            )
-    ) {
-        WatchaBasicTextField(
-            placeholder = placeholder,
-            value = value,
-            onValueChange = onValueChange,
-            textStyle = WatchaTheme.typography.cap.captionR14,
-            textFieldColor = textFieldStyle.getTextFieldColor(),
-            disabled = !enabled,
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 17.dp),
-            keyboardOptions = keyboardOptions,
-            visualTransformation = visualTransformation,
+    Column(modifier = modifier) {
+        Text(
+            text = label,
+            style = WatchaTheme.typography.cap.captionR14,
+            color = WatchaTheme.colors.textSecondary,
         )
+        Spacer(modifier = Modifier.height(3.dp))
+
+        Box(
+            modifier = Modifier
+                .background(
+                    color = textFieldStyle.getTextFieldColor().backgroundColor,
+                    shape = RoundedCornerShape(8.dp),
+                )
+        ) {
+            WatchaBasicTextField(
+                placeholder = placeholder,
+                value = value,
+                onValueChange = onValueChange,
+                textStyle = WatchaTheme.typography.cap.captionR14,
+                textFieldColor = textFieldStyle.getTextFieldColor(),
+                disabled = !enabled,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 17.dp),
+                keyboardOptions = keyboardOptions,
+                visualTransformation = visualTransformation,
+            )
+        }
     }
 }
 
@@ -61,6 +81,7 @@ private fun BasicTextFieldPreview() {
             value = text,
             onValueChange = { text = it },
             modifier = Modifier,
+            label = "이메일",
         )
     }
 }
