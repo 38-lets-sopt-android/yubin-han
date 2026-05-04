1ffdec17c63cdb7d4788e6f1be453b6a3bcf4c70
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-04 10:57:33 +0900

    [refactor/#6] 유효성 검증 로직 수정 및 에러메세지 추가

--- app/src/main/java/com/example/letssopt/core/util/AuthValidator.kt
+++ app/src/main/java/com/example/letssopt/core/util/AuthValidator.kt
@@ -6,16 +6,29 @@ object AuthValidator {
     private val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$".toRegex()
 
     fun validateSignUp(
+        id: String,
         email: String,
         pw: String,
-        pwConfirm: String
+        pwConfirm: String,
+        name: String,
+        age: String,
+        part: String
     ): ValidationErrorType? {
-        val emailMatcher = email.matches(emailRegex)
-        if (!emailMatcher || pw.length !in 8..12) {
-            return ValidationErrorType.INVALID_FORMAT
+        if (id.isBlank() || email.isBlank() || pw.isBlank() || pwConfirm.isBlank() || name.isBlank() || age.isBlank() || part.isBlank()) {
+            return ValidationErrorType.EMPTY_INPUT
         }
         if (pw != pwConfirm) {
             return ValidationErrorType.PASSWORD_MISMATCH
+        }
+        val emailMatcher = email.matches(emailRegex)
+        if (!emailMatcher) {
+            return ValidationErrorType.EMAIL_INVALID_FORMAT
+        }
+        if (pw.length !in 8..12) {
+            return ValidationErrorType.PW_INVALID_FORMAT
+        }
+        if (age.toIntOrNull() !in 1..150) {
+            return ValidationErrorType.AGE_INVALID_FORMAT
         }
         return null
     }
