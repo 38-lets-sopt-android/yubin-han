afc25181a2320fccf502e9721468291ea32222a9
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-05 09:17:16 +0900

    [mod/#8] contract Immutable

--- app/src/main/java/com/example/letssopt/presentation/home/HomeContract.kt
+++ app/src/main/java/com/example/letssopt/presentation/home/HomeContract.kt
@@ -1,8 +1,10 @@
 package com.example.letssopt.presentation.home
 
+import androidx.compose.runtime.Immutable
 import com.example.letssopt.core.data.model.home.MainHomeItem
 
 interface HomeContract {
+    @Immutable
     data class UiState(
         val homeItems: List<MainHomeItem> = emptyList()
     )
