6b27777df659097f84a591848d8aff4e45f62bcb
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-05 13:55:33 +0900

    [feat/#8] UserProfile model 구현

---
+++ app/src/main/java/com/example/letssopt/core/data/model/home/myprofile/UserProfile.kt
@@ -0,0 +1,9 @@
+package com.example.letssopt.core.data.model.home.myprofile
+
+data class UserProfile(
+    val id: String,
+    val name: String,
+    val email: String,
+    val age: Int,
+    val part: String
+)
