74dd3e746074bc57fa7dcdae401afbc4b8a8383c
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-07 07:38:49 +0900

    [feat/#8] 개인정보 조회 API dto 구현

---
+++ app/src/main/java/com/example/letssopt/core/data/dto/GetUserProfileResponse.kt
@@ -0,0 +1,34 @@
+package com.example.letssopt.core.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetUserProfileResponse(
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("code")
+    val code: String,
+    @SerialName("data")
+    val data: UserProfileData? = null,
+)
+
+@Serializable
+data class UserProfileData(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("loginId")
+    val logInId: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("email")
+    val email: String,
+    @SerialName("age")
+    val age: Int,
+    @SerialName("part")
+    val part: String,
+)
