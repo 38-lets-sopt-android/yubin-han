24591edefb33cca31b3c07f4b73c6a55b4186913
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-06 19:00:57 +0900

    [feat/#8] 로그인 API dto 구현

---
+++ app/src/main/java/com/example/letssopt/core/data/dto/PostSignInResponse.kt
@@ -0,0 +1,25 @@
+package com.example.letssopt.core.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class PostSignInResponse(
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("code")
+    val code: String,
+    @SerialName("data")
+    val data: UserData,
+)
+
+@Serializable
+data class UserData(
+    @SerialName("userId")
+    val userId: Int,
+)
