4f77b019aef2558a939c7835f02410f776c9cf50
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-06 19:07:46 +0900

    [refactor/#8] API 연동에 따른 에러메세지 간략호

--- app/src/main/java/com/example/letssopt/presentation/onboarding/login/LoginViewModel.kt
+++ app/src/main/java/com/example/letssopt/presentation/onboarding/login/LoginViewModel.kt
@@ -2,7 +2,6 @@ package com.example.letssopt.presentation.onboarding.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.letssopt.core.data.repository.api.AuthException
 import com.example.letssopt.core.data.repository.api.AuthRepository
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -32,19 +31,13 @@ class LoginViewModel(
     fun login() {
         viewModelScope.launch {
             val currentState = _uiState.value
-            userRepository.login(currentState.idText, currentState.pwText)
+            userRepository.signIn(currentState.idText, currentState.pwText)
                 .onSuccess {
                     _effect.send(LoginContract.Effect.ShowToast("로그인에 성공했습니다"))
                     _effect.send(LoginContract.Effect.NavigateToMain)
                 }
                 .onFailure { error ->
-                    val message = when (error) {
-                        is AuthException.InvalidCredentials ->
-                            "이메일 또는 비밀번호가 일치하지 않습니다."
-
-                        else -> "로그인 중 오류가 발생했습니다."
-                    }
-                    _effect.send(LoginContract.Effect.ShowToast(message))
+                    _effect.send(LoginContract.Effect.ShowToast(error.message ?: "로그인 중 오류가 발생했습니다."))
                 }
         }
     }
