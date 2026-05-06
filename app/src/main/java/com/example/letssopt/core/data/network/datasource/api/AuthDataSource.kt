380ef4a304234ed8c93e5856de2213b17427acd4
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-06 19:01:49 +0900

    [feat/#8] 로그인 AuthDataSource 구현

--- app/src/main/java/com/example/letssopt/core/data/network/datasource/api/AuthDataSource.kt
+++ app/src/main/java/com/example/letssopt/core/data/network/datasource/api/AuthDataSource.kt
@@ -1,9 +1,13 @@
 package com.example.letssopt.core.data.network.datasource.api
 
+import com.example.letssopt.core.data.dto.PostSignInRequest
+import com.example.letssopt.core.data.dto.PostSignInResponse
 import com.example.letssopt.core.data.dto.PostSignUpRequest
 import com.example.letssopt.core.data.dto.PostSignUpResponse
 import retrofit2.Response
 
 interface AuthDataSource {
     suspend fun signUp(request: PostSignUpRequest): Response<PostSignUpResponse>
+
+    suspend fun signIn(request: PostSignInRequest): Response<PostSignInResponse>
 }
--- app/src/main/java/com/example/letssopt/core/data/network/datasource/impl/AuthDataSourceImpl.kt
+++ app/src/main/java/com/example/letssopt/core/data/network/datasource/impl/AuthDataSourceImpl.kt
@@ -1,15 +1,21 @@
 package com.example.letssopt.core.data.network.datasource.impl
 
+import com.example.letssopt.core.data.dto.PostSignInRequest
+import com.example.letssopt.core.data.dto.PostSignInResponse
 import com.example.letssopt.core.data.dto.PostSignUpRequest
 import com.example.letssopt.core.data.dto.PostSignUpResponse
 import com.example.letssopt.core.data.network.datasource.api.AuthDataSource
 import com.example.letssopt.core.data.network.service.AuthService
 import retrofit2.Response
 
-class AuthRemoteDataSourceImpl(
+class AuthDataSourceImpl(
     private val authService: AuthService
 ) : AuthDataSource {
     override suspend fun signUp(request: PostSignUpRequest): Response<PostSignUpResponse> {
         return authService.signUp(request)
+    }
+
+    override suspend fun signIn(request: PostSignInRequest): Response<PostSignInResponse> {
+        return authService.signIn(request)
     }
 }
