cf9e45bace3dee0cc8043ab01386e1643dd2adab
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-07 09:50:07 +0900

    [mod/#8] UserProfile 이메일 나이 선택인자

--- app/src/main/java/com/example/letssopt/core/data/model/profile/UserProfile.kt
+++ app/src/main/java/com/example/letssopt/core/data/model/profile/UserProfile.kt
@@ -3,7 +3,7 @@ package com.example.letssopt.core.data.model.profile
 data class UserProfile(
     val id: String,
     val name: String,
-    val email: String,
-    val age: Int,
+    val email: String = "",
+    val age: Int = 0,
     val part: String
 )
