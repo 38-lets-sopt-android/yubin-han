da83f11f65d764a196b3028dd105081b5a48d7f3
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-07 09:52:06 +0900

    [feat/#8] 최근 가입 유저 리스트 API dto 구현

---
+++ app/src/main/java/com/example/letssopt/core/data/dto/GetRecentUsersResponse.kt
@@ -0,0 +1,34 @@
+package com.example.letssopt.core.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetRecentUsersResponse(
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("code")
+    val code: String,
+    @SerialName("data")
+    val data: RecentUsersDataList? = null,
+)
+
+@Serializable
+data class RecentUsersDataList(
+    @SerialName("users")
+    val users: List<RecentUsersData>
+)
+
+@Serializable
+data class RecentUsersData(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("name")
+    val name: String,
+    @SerialName("part")
+    val part: String,
+)
